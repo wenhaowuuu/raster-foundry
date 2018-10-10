8fa88794e1937b7ee8788f949809e0e5a03f958d
Chris Brown <cbrown@azavea.com>
2018-10-10 14:15:24 -0400

    WIP

--- app-backend/backsplash/src/main/scala/services/HealthCheckService.scala
+++ app-backend/backsplash/src/main/scala/services/HealthCheckService.scala
@@ -5,11 +5,21 @@ import io.circe.Json
 import org.http4s.HttpService
 import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
+import com.amazonaws.xray.AWSXRay
+
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.handlers.TracingHandler;
+
 
 class HealthCheckService[F[_]: Effect] extends Http4sDsl[F] {
   val service: HttpService[F] = {
     HttpService[F] {
       case GET -> Root => {
+        val segment = AWSXRay.beginSegment("Test")
+        segment.addSubsegment()
+        val subsegment = AWSXRay.beginSubsegment("Starting Healthcheck")
+        Thread.sleep(4000)
+        subsegment.end()
         Ok(
           Json.obj("message" -> Json.fromString("Healthy"),
                    "reason" -> Json.fromString("A-ok")))
